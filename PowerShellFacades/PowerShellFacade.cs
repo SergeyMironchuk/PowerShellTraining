4da5516bc3f0f26f860a4ac22a5cee26867708e3
Administrator <user@example.com>
2016-06-25 22:42:50 +0300

    PowerShell Facade created

---
+++ PowerShellFacades/PowerShellFacade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace PowerShellFacades
+{
+    public class PowerShellFacade
+    {
+        public void RunCommand(string commandName, IEnumerable<KeyValuePair<string, object>> parameters, Action<Collection<PSObject>> processComandResult)
+        {
+            using (PowerShell powershell = PowerShell.Create())
+            {
+                powershell.AddCommand(commandName);
+                foreach (var parameter in parameters)
+                {
+                    powershell.AddParameter(parameter.Key, parameter.Value);
+                }
+                Collection<PSObject> results = powershell.Invoke();
+
+                if (processComandResult != null) processComandResult(results);
+            }
+        }
+    }
+}
---
+++ TrainingProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using PowerShellFacades;
+
+namespace TrainingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var powerShell = new PowerShellFacade();
+            powerShell.RunCommand("dir", new KeyValuePair<string, object>[0], objects =>
+            {
+                foreach (var psObject in objects)
+                {
+                    Console.WriteLine(psObject.ToString());
+                }
+            });
+        }
+    }
+}
