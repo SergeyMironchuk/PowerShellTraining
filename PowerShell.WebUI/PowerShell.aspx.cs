25beed7ad7d48b82947df2e16310e009e1dcf554
Sergey Mironchuk <sergey@mironchuk.in.ua>
2016-07-15 11:12:40 +0300

    Last changes

---
+++ PowerShell.WebUI/PowerShell.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PowerShell.WebUI
+{
+    public partial class PowerShell : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
